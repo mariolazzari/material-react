be617af6c32f66a79c2660368ba76418c61b77ec
Mario Lazzari <mario.lazzari@gmail.com>
2020-02-14 10:10:43 +0100

    Done lesson 13

---
+++ arcdevelopment/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/ui/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Ciao ciao
+    </div>
+  );
+}
+
+export default App;
---
+++ arcdevelopment/src/components/ui/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  });
+}
+
+function Header() {
+  return (
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
+
+export default Header;
